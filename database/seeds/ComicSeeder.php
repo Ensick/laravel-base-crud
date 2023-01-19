028a476a069a9289b47eb915c19a3d1dba844aee
Ensick <fede.censi96@gmail.com>
2023-01-19 16:33:06 +0100

    Stampa tabella database nella pagina index

---
+++ database/seeds/ComicSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Models\Comic;
+
+class ComicSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $array_comics = config('comics');
+
+        foreach($array_comics as $elem){
+
+            $new_comic = new Comic();
+            $new_comic ->title = $elem['title'];
+            $new_comic ->description = $elem['description'];
+            $new_comic ->thumb = $elem['thumb'];
+            $new_comic ->price = $elem['price'];
+            $new_comic ->series = $elem['series'];
+            $new_comic ->sale_date = $elem['sale_date'];
+            $new_comic ->type = $elem['type'];
+            $new_comic ->save();
+
+        }
+    }
+}
--- resources/views/pages/comics/index.blade.php
+++ resources/views/pages/comics/index.blade.php
@@ -4,4 +4,35 @@
 
 @section('main-content')
     <h1>Lista Comics</h1>
+
+    <table class="table">
+        <thead>
+          <tr>
+            <th scope="col">id</th>
+            <th scope="col">Titolo</th>
+            <th scope="col">Descrizione</th>
+            <th scope="col">Copertina</th>
+            <th scope="col">Prezzo</th>
+            <th scope="col">Serie</th>
+            <th scope="col">Uscita</th>
+            <th scope="col">Genere</th>
+          </tr>
+        </thead>
+        <tbody>
+
+            @foreach ($comics as $elem)
+            <tr>
+                <td>{{$elem->id}}</td>
+                <td>{{$elem->title}}</td>
+                <td>{{$elem->description}}</td>
+                <td> <img src="{{$elem->thumb}}" alt="img"> </td>
+                <td>{{$elem->price}}</td>
+                <td>{{$elem->series}}</td>
+                <td>{{$elem->sale_date}}</td>
+                <td>{{$elem->type}}</td>
+            </tr>
+            @endforeach
+
+        </tbody>
+      </table>
 @endsection
